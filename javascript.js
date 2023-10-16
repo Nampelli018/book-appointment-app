84272799f73822b088914096a60d2fb9fe79eaba
Nampelli018 <nampallyvamshi1@gmail.com>
2023-10-16 14:25:27 +0530

    Get the saved User Details from crudcrud.

--- javascript.js
+++ javascript.js
@@ -1,39 +1,20 @@
 $(document).ready(function () {
-  var savedCallData = JSON.parse(localStorage.getItem("callDataArray")) || [];
+  var savedCallData = [];
 
   // Function to add an appointment to the table
-  function addAppointmentToTable(appointment) {
+  function addAppointmentToTable(appointment, index) {
     // Create a new row in the table
     let newRow = document.createElement("tr");
     newRow.innerHTML = `
       <td>${appointment.name}</td>
       <td>${appointment.email}</td>
       <td>${appointment.phone}</td>
-      <td><button class="edit">Edit</button></td>
-      <td><button class="delete">Delete</button></td>
+      <td><button class="edit" data-index="${index}">Edit</button></td>
+      <td><button class="delete" data-index="${index}">Delete</button></td>
     `;
 
     // Append the new row to the table
     tableBody.appendChild(newRow);
-
-    const editButtons = newRow.querySelectorAll(".edit");
-    editButtons.forEach(function (button) {
-      button.addEventListener("click", function () {
-        const index = Array.from(newRow.parentElement.children).indexOf(newRow);
-        editIndex = index; // Set the edit index
-        populateFormWithOldData(index);
-        submitButton.textContent = "Edit";
-      });
-    });
-
-    const deleteButtons = newRow.querySelectorAll(".delete");
-    deleteButtons.forEach(function (button) {
-      button.addEventListener("click", function () {
-        const index = Array.from(newRow.parentElement.children).indexOf(newRow);
-        deleteAppointment(index);
-        updateTable();
-      });
-    });
   }
 
   const form = document.getElementById("myForm");
@@ -44,7 +25,6 @@ $(document).ready(function () {
   const callTimeInput = document.getElementById("callTime");
   const submitButton = document.getElementById("submit");
   const tableBody = document.getElementById("tableBody");
-  let editIndex = -1; // Track the index for editing
 
   submitButton.addEventListener("click", function () {
     const name = nameInput.value;
@@ -54,50 +34,41 @@ $(document).ready(function () {
     const callTime = callTimeInput.value;
 
     if (name && email && phone && callData && callTime) {
-      const rowData = [name, email, phone, callData, callTime];
+      const appointment = {
+        name: name,
+        email: email,
+        phone: phone,
+        callData: callData,
+        callTime: callTime,
+      };
 
-      if (editIndex !== -1) {
-        editInLocalStorage(rowData, editIndex);
-        editIndex = -1; // Reset the edit index
-        submitButton.textContent = "Submit";
-      } else {
-        // Send the data to Crudcrud
-        sendToCrudcrud(rowData);
-      }
+      // Add the appointment to the table and send it to Crudcrud
+      addAppointmentToTable(appointment, savedCallData.length);
+      sendToCrudcrud(appointment);
 
-      updateTable();
+      // Reset the form
       clearFormInputs();
     } else {
       alert("Please fill in all fields.");
     }
   });
 
-  function addToLocalStorage(data) {
-    savedCallData.push(data);
-    localStorage.setItem("callDataArray", JSON.stringify(savedCallData));
-  }
-
-  function editInLocalStorage(data, index) {
-    savedCallData[index] = data;
-    localStorage.setItem("callDataArray", JSON.stringify(savedCallData));
-  }
-
   function deleteAppointment(index) {
     savedCallData.splice(index, 1);
-    localStorage.setItem("callDataArray", JSON.stringify(savedCallData));
+    updateTable();
   }
 
   function updateTable() {
-    const data = savedCallData;
-
     let tableHTML = "";
-    data.forEach(function (row, index) {
+    savedCallData.forEach(function (row, index) {
       tableHTML += "<tr>";
-      row.forEach(function (cell) {
-        tableHTML += `<td>${cell}</td>`;
-      });
+      tableHTML += `<td>${row.name}</td>`;
+      tableHTML += `<td>${row.email}</td>`;
+      tableHTML += `<td>${row.phone}</td>`;
+      tableHTML += `<td>${row.callData} ${row.callTime}</td>`;
       tableHTML += `<td><button class="edit" data-index="${index}">Edit</button></td>`;
-      tableHTML += `<td><button class="delete" data-index="${index}">Delete</button></td></tr>`;
+      tableHTML += `<td><button class="delete" data-index="${index}">Delete</button></td>`;
+      tableHTML += "</tr>";
     });
 
     tableBody.innerHTML = tableHTML;
@@ -106,9 +77,11 @@ $(document).ready(function () {
     editButtons.forEach(function (button) {
       button.addEventListener("click", function () {
         const index = parseInt(button.getAttribute("data-index"));
-        editIndex = index; // Set the edit index
         populateFormWithOldData(index);
         submitButton.textContent = "Edit";
+
+        // Remove the row for editing
+        tableBody.removeChild(button.parentElement.parentElement);
       });
     });
 
@@ -117,19 +90,18 @@ $(document).ready(function () {
       button.addEventListener("click", function () {
         const index = parseInt(button.getAttribute("data-index"));
         deleteAppointment(index);
-        updateTable();
       });
     });
   }
 
   function populateFormWithOldData(index) {
-    const data = savedCallData;
-    if (data[index]) {
-      nameInput.value = data[index][0];
-      emailInput.value = data[index][1];
-      phoneInput.value = data[index][2];
-      callDataInput.value = data[index][3];
-      callTimeInput.value = data[index][4];
+    const data = savedCallData[index];
+    if (data) {
+      nameInput.value = data.name;
+      emailInput.value = data.email;
+      phoneInput.value = data.phone;
+      callDataInput.value = data.callData;
+      callTimeInput.value = data.callTime;
     }
   }
 
@@ -139,6 +111,7 @@ $(document).ready(function () {
     phoneInput.value = "";
     callDataInput.value = "";
     callTimeInput.value = "";
+    submitButton.textContent = "GET A CALL";
   }
 
   // Send the data to Crudcrud
@@ -147,37 +120,27 @@ $(document).ready(function () {
       "https://crudcrud.com/api/307db7265d1245e6abf490120ab1f1fe/appointmentappData"; // Replace with your actual Crudcrud API endpoint
 
     axios
-      .post(crudcrudEndpoint, { data: data })
+      .post(crudcrudEndpoint, data)
       .then((response) => {
         // Handle the response if needed
+        savedCallData.push(data); // Add data to the array
+        updateTable(); // Update the table after receiving a response
       })
       .catch((err) => {
         console.error("Error sending data to Crudcrud:", err);
       });
   }
 
-  // Add an event listener to the form to handle appointment submissions
-  form.addEventListener("submit", function (event) {
-    event.preventDefault(); // Prevent form submission
-
-    const name = nameInput.value;
-    const email = emailInput.value;
-    const phone = phoneInput.value;
-
-    if (name && email && phone) {
-      const appointment = {
-        name: name,
-        email: email,
-        phone: phone,
-      };
-
-      // Add the appointment to the table
-      addAppointmentToTable(appointment);
-
-      // Reset the form
-      clearFormInputs();
-    } else {
-      alert("Please fill in all appointment fields.");
-    }
-  });
+  // Load saved data on page load
+  axios
+    .get(
+      "https://crudcrud.com/api/307db7265d1245e6abf490120ab1f1fe/appointmentappData"
+    )
+    .then((response) => {
+      savedCallData = response.data;
+      updateTable();
+    })
+    .catch((err) => {
+      console.error("Error fetching data from Crudcrud:", err);
+    });
 });
